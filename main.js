f023b10b8542f1920d90926c9e1ee5795f6573e0
Alex Welsh <alex.n.welsh@gmail.com>
2016-06-28 08:47:29 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Kyle Simpson, front end masters.  Lecture Advanced JS tutorial
+
+
+function Bar(who) {
+
+	this.me = who;
+
+}
+
+Bar.prototype = a1;
+
+var b1 = new Bar("b1");
+
+b1.identify();
